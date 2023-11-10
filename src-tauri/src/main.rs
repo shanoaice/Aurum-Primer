2836851d098f44afbb43daf7b475a8983c7116f7
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-11-10 23:08:30 +0800

    feature(singbox-tauri): process manager

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod singbox_manager;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let initial_singbox_process = singbox_manager::SingBox::new();
+
+    tauri::Builder::default()
+        .manage(initial_singbox_process)
+        .invoke_handler(tauri::generate_handler![
+            singbox_manager::start_singbox_process_with,
+            singbox_manager::start_singbox_process,
+            singbox_manager::set_singbox_parameters,
+            singbox_manager::reload_singbox_process,
+            singbox_manager::stop_singbox_process
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
