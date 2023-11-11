e75678d0e6d645cd5a125b2d839b19dca0846d42
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-11-11 11:00:45 +0800

    feature(config): v2ray types

--- src/libs/v2ray.ts
+++ src/libs/v2ray.ts
@@ -33,15 +33,53 @@ type V2RayGrpcTranport = [
 ];
 
 type V2RayHttpUpgradeTranport = [
-	'http',
+	'httpupgrade',
 	{
 		host: string[];
 		path: string;
-		method: string;
 		headers: Record<string, string>;
-		idle_timeout: string;
-		ping_timeout: string;
 	},
 ];
 
-type V2RayTransportType = 'http' | 'ws' | 'quic' | 'grpc' | 'httpupgrade';
+type V2RayTransports =
+	| V2RayHttpTranport
+	| V2RayWebSocketTranport
+	| V2RayQuicTranport
+	| V2RayGrpcTranport
+	| V2RayHttpUpgradeTranport;
+
+export function createV2RayTransport(transport: V2RayTransports) {
+	return {
+		type: transport[0],
+		...transport[1],
+	};
+}
+
+type V2RaySecurity =
+	| 'none'
+	| 'auto'
+	| 'zero'
+	| 'aes-128-gcm'
+	| 'chacha20-poly1305';
+
+type V2RayPacketEncoding = undefined | 'packetaddr' | 'xudp';
+
+export function createV2RayOutbound(
+	server: string,
+	port: number,
+	security: V2RaySecurity,
+	uuid: string,
+	transport: V2RayTransports,
+	legacy = false,
+	packetEncoding?: V2RayPacketEncoding
+) {
+	return {
+		server,
+		server_port: port,
+		security,
+		uuid,
+		alter_id: legacy ? 1 : 0,
+		packet_encoding: packetEncoding,
+		transport: createV2RayTransport(transport),
+	};
+}
