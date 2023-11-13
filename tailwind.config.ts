bacc4edcd013920d478ae7611796e3a6ba77fa8b
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-11-13 10:02:30 +0800

    chore(build): tailwind icons config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import daisyui from 'daisyui';
 import { type Config } from 'tailwindcss';
+import { addDynamicIconSelectors } from '@iconify/tailwind';
 
 const tailwindConfig: Config = {
 	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui],
+	plugins: [daisyui, addDynamicIconSelectors()],
 	daisyui: {
 		themes: true,
 	},
