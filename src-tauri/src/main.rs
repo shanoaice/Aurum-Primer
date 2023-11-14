b3ba951189d1952d7995aac6b42ee5c1766bf22b
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-11-14 09:54:32 +0800

    chore(plugins-tauri): register tauri store plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![feature(ip_bits)]
 
-mod singbox_manager;
-mod random_cidr_generator;
 mod os_directories;
+mod random_cidr_generator;
+mod singbox_manager;
 
 fn main() {
     let initial_singbox_process = singbox_manager::SingBox::new();
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::default().build())
         .manage(initial_singbox_process)
         .invoke_handler(tauri::generate_handler![
             singbox_manager::start_singbox_process_with,
@@ -17,10 +18,10 @@ fn main() {
             singbox_manager::set_singbox_parameters,
             singbox_manager::reload_singbox_process,
             singbox_manager::stop_singbox_process,
-			random_cidr_generator::generate_random_ipv4_local_30,
-			random_cidr_generator::generate_random_ipv6_local_126,
-			os_directories::get_data_dir,
-			os_directories::get_config_dir
+            random_cidr_generator::generate_random_ipv4_local_30,
+            random_cidr_generator::generate_random_ipv6_local_126,
+            os_directories::get_data_dir,
+            os_directories::get_config_dir
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
