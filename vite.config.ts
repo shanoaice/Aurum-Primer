4567324ab4199589570b0bed49ac74a9f22c8d1d
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-11-14 20:00:07 +0800

    config(build): aliases and top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solid()],
-})
+	base: './',
+	resolve: { alias: { '~': '/src' } },
+	plugins: [solid()],
+	build: {
+		// Enables top-level await (this :( requires ES2022)
+		target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'],
+	},
+});
