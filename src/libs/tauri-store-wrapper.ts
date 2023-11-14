1e3d664b5f6947deb72d9570f5d6567eb2fb06ea
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-11-14 20:01:12 +0800

    feature(tauri-store): AsyncStore wrapper for tauri store

--- src/libs/tauri-store-wrapper.ts
+++ src/libs/tauri-store-wrapper.ts
@@ -4,7 +4,10 @@ import { type AsyncStorage } from '@solid-primitives/storage';
 export const createTauriStoreApi = (tauriStore: TauriStore): AsyncStorage => ({
 	getItem: async (key: string) => tauriStore.get(key),
 	getAll: async () => tauriStore.entries(),
-	setItem: async (key: string, value: string) => tauriStore.set(key, value),
+	async setItem(key: string, value: string) {
+		console.log(`set ${key} to ${value}`);
+		return tauriStore.set(key, value);
+	},
 	removeItem(key) {
 		void tauriStore.delete(key);
 	},
--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,8 @@ import { path } from '@tauri-apps/api';
 
 const tauriStorePath = await path.join(await path.appDataDir(), 'data.json');
 
+console.log(tauriStorePath);
+
 export const tauriStore = new Store(tauriStorePath);
 
-export const asyncStoreApiTauri = createTauriStoreApi(tauriStore);
+export const asyncTauriStoreApi = createTauriStoreApi(tauriStore);
