99abc6e195d3f2a83c34e9592c8438e1660b029b
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-12-06 10:07:07 +0800

    chore(build): no swc for transformation
    unfortunately esbuild minification doesn't play well with swc-generated chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
-import swc from 'unplugin-swc';
 
 export default defineConfig({
 	base: './',
 	resolve: { alias: { '~': '/src' } },
 	plugins: [
-		swc.vite(),
 		devtools({
 			autoname: true,
 		}),
